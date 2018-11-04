e1c4da6eb6bd82b94c6b32d52f869e1c1507d7c7
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-11-04 23:44:42 +0100

    Clean up

--- script.js
+++ script.js
@@ -21,30 +21,25 @@ var waitTime = 330;
 
 async function loadJsonFile() {
     const response = await fetch("hullapelyhes.json");
-    const parsedPromise = await response.json();
-    console.log("Response has been parsed: [" + parsedPromise + "] from response [" + response + "]");
-    return parsedPromise;
+    return await response.json();
 }
 
 async function playMusic() {
     var song = await loadJsonFile();
-    console.log(song.length);
+
     for (var i = 0; i < song.length; i++) {
         var sound = song[i]['sound'];
         var soundSource = soundFiles[sound];
-        console.log(soundSource);
         var audio = new Audio(soundSource);
         await audio.play();
 
         var delayMs = song[i]['delay'] * waitTime;
-        console.log(delayMs);
-        await wait(delayMs);
+        wait(delayMs);
     }
 }
 
 $(document).ready(function(){
     $("button").click(function(){
-        console.log("szarfostökmindegy");
-            playMusic();
+        playMusic();
     });
 });
