29eb5ac3766aa8cbbf56a70f9fa02c805be70805
Rudolf Ádám <sari.vogel.94@gmail.com>
2018-11-04 23:24:24 +0100

    add js and jscript, play the song "hullapelyhes"

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 
+function wait(ms)
+{
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+var soundFiles = {
+    "c": "sound/synth_c_note.wav",
+    "d": "sound/synth_d_note.wav",
+    "e": "sound/synth_e_note.wav",
+    "f": "sound/synth_f_note.wav",
+    "g": "sound/synth_g_note.wav",
+    "a": "sound/synth_a_note.wav",
+    "h": "sound/synth_h_note.wav"
+}
+
+var waitTime = 330;
+
+async function loadJsonFile() {
+    const response = await fetch("hullapelyhes.json");
+    const parsedPromise = await response.json();
+    console.log("Response has been parsed: [" + parsedPromise + "] from response [" + response + "]");
+    return parsedPromise;
+}
+
+async function playMusic() {
+    var song = await loadJsonFile();
+    console.log(song.length);
+    for (var i = 0; i < song.length; i++) {
+        var sound = song[i]['sound'];
+        var soundSource = soundFiles[sound];
+        console.log(soundSource);
+        var audio = new Audio(soundSource);
+        await audio.play();
+
+        var delayMs = song[i]['delay'] * waitTime;
+        console.log(delayMs);
+        await wait(delayMs);
+    }
+}
+
 $(document).ready(function(){
     $("button").click(function(){
-            var mittLjud = document.getElementById('mittLjud');
-            mittLjud.play();
+        console.log("szarfostökmindegy");
+            playMusic();
     });
 });
